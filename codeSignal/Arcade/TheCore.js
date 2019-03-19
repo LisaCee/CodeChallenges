26d3b154dc1f60238026e08cd02aaf65b1f1ce48
LisaCee <lisaceedesign@gmail.com>
2019-03-18 19:48:25 -0700

    maxMultiple

--- codeSignal/Arcade/TheCore.js
+++ codeSignal/Arcade/TheCore.js
@@ -1,20 +1,32 @@
 //## Intro Gates
 
 function addTwoDigits(n) {
-    return n.toString().split('').reduce((a, b) => {
-        return parseInt(a) + parseInt(b);
-    })
+  return n
+    .toString()
+    .split("")
+    .reduce((a, b) => {
+      return parseInt(a) + parseInt(b);
+    });
 }
 function largestNumber(n) {
   return parseInt("9".repeat(n));
 }
 function candies(n, m) {
-    return Math.floor(m / n) * n
+  return Math.floor(m / n) * n;
 }
 function seatsInTheater(nCols, nRows, col, row) {
-    return (nCols - col + 1) * (nRows - row)
+  return (nCols - col + 1) * (nRows - row);
 }
-
+function maxMultiple(divisor, bound) {
+  let solution = bound;
+  while (solution => 1) {
+    if (solution % divisor == 0) {
+      return solution;
+    } else {
+      solution--;
+    }
+  }
+}
 
 function reachNextLevel(experience, threshold, reward) {
   return experience + reward >= threshold;
