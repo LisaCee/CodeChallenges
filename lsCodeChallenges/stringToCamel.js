078752aec85a61b9762430fe33c347b85be3a42f
LisaCee <lisaceedesign@gmail.com>
2019-04-04 14:46:52 -0700

    string to camel case 2.0 w/map

--- lsCodeChallenges/stringToCamel.js
+++ lsCodeChallenges/stringToCamel.js
@@ -1,13 +1,23 @@
+// const stringToCamelCase = str => {
+//     if (str.includes('-')) {
+//         str = str.split('-')
+//     } else if (str.includes('_')) {
+//         str = str.split('_')
+//     }
+//     for (let i = 0; i < str.length; i++) {
+//         str[i] = str[i][0].toUpperCase() + str[i].slice(1)
+//     } 
+//     return str.join(' ');
+// }
+
 const stringToCamelCase = str => {
     if (str.includes('-')) {
-        str = str.split('-')
-    } else if (str.includes('_')) {
-        str = str.split('_')
-    }
-    for (let i = 0; i < str.length; i++) {
-        str[i] = str[i][0].toUpperCase() + str[i].slice(1)
-    } 
-    return str.join(' ');
+            str = str.split('-')
+        } else if (str.includes('_')) {
+            str = str.split('_')
+        }
+
+    return str.map((word) => word = word.charAt(0).toUpperCase() + word.slice(1)).join(' ')     
 }
 
 console.log(stringToCamelCase('hi-my-name-is-lisa'))
